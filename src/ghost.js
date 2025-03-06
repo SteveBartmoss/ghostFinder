922ee86b65b82c06310b2d789cdc89743f751619
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-03-06 15:36:08 -0600

    feat
    Se agrego un archivo para manejar la llama a la api de busqueda

---
+++ src/ghost.js
@@ -0,0 +1,20 @@
+export async function daemonSearch(query){
+
+  const url = `htpps://searxng.org/search?=${encodeURIComponent(query)}&format=json`
+
+  try{
+
+    const response = await fetch(url)
+    if(!response.ok){
+      throw new Error(`Error: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data.results
+    
+  }catch(error){
+
+    console.error("Error al realizar la busqueda: ",error.message)
+    
+  }
+}
