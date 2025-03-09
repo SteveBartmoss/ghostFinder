9ad8964df332a5942045e9c651d15fc21325b79b
steve <za19012397@zapopan.tecmm.edu.mx>
2025-03-08 21:51:47 -0600

    feat: Se reemplazo el api de searx por duckduckgo

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ import readline from 'readline';
 
 export async function buscarEnSearx(query) {
   
-  const url = `https://searx.be/search?q=${encodeURIComponent(query)}&format=json`;
+  const url = `https://api.duckduckgo.com/?q=${encodeURIComponent(query)}&format=json`;   
 
   try {
 
     const response = await fetch(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-      }
+      headers: { 'User-Agent': 'Mozilla/5.0' }
     });
     
     console.log(response)
